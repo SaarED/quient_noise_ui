28d760fe73174d2f8b389038b320959f67d12ca8
SaarED <ss2102xz@gmail.com>
2019-08-08 01:34:17 +0300

    Saar - Creating Channel page ui

--- src/app/channel/channel.component.ts
+++ src/app/channel/channel.component.ts
@@ -8,18 +8,42 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ChannelComponent implements OnInit {
 
+  record = false;
   channel = null;
 
   constructor(private route: ActivatedRoute) {
-    this.route.params.subscribe( params => this.channel = params.id );
+    this.route.params.subscribe( params => this.channel = {
+        id: params.id,
+        properties: [
+          { name: 'param 1', data: '123', confidence: 0.8 },
+          { name: 'param 2', data: '123', confidence: 0.8 },
+          { name: 'param 3', data: '123', confidence: 0.8 },
+          { name: 'param 4', data: '123', confidence: 0.8 },
+          { name: 'param 5', data: '123', confidence: 0.8 },
+          { name: 'param 1', data: '123', confidence: 0.8 },
+          { name: 'param 2', data: '123', confidence: 0.8 },
+          { name: 'param 3', data: '123', confidence: 0.8 },
+          { name: 'param 4', data: '123', confidence: 0.8 },
+          { name: 'param 5', data: '123', confidence: 0.8 },
+        ]
+      }
+    );
   }
 
   ngOnInit() {
     this.route.paramMap.subscribe(parmaps => {
       if(parmaps) {
-        data => this.channel = data 
+        data => this.channel = data
       }
     });
+  }
+
+  startRecord() {
+    this.record = true;
+  }
+
+  stopRecord() {
+    this.record = false;
   }
 
 }
